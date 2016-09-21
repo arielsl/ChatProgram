8633b7cbd92c59cec87f204cdfdbe7cb1021afe3
thatboytitz <thatboytitz@users.noreply.github.com>
2016-09-20 18:50:46 -0700

    Update ServerClient.java

--- ServerClient.java
+++ ServerClient.java
@@ -1,97 +1,76 @@
-package chat;
+package com.company;
 
+import javax.swing.*;
 import java.net.*;
-import java.util.Scanner;
 import java.io.*;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
-public class ServerClient extends Thread {
-   private ServerSocket serverSocket;
-   
-   //Use this class method to create a socket with the given port
-   //Add a timeout in case of standby
-   public ServerClient(int port) throws IOException {
-      serverSocket = new ServerSocket(port);
-      serverSocket.setSoTimeout(30000);
-   }
-   
-   public static void main(String [] args) {
-	  //Get the port number from the arguments and parse it as an integer
-      int port = Integer.parseInt(args[0]);
-      //Setup variables for the loop to create the server socket
-      int createServer = 0;
-      int newPort = 0;
-      int portMethod = 0;
-      //Open a scanner to read input
-      Scanner sc = new Scanner(System.in);
-      
-      //If there is no socket created, use 0 and enter the loop to create one
-      while (createServer == 0){
-	      
-    	  //Try and catch to create a socket
-    	  try {
-    		 ///If it is the initial creation, use the console arguments given
-    		 if (portMethod == 0){
-		         Thread t = new ServerClient(port);
-		         t.start();
-		         createServer = 1;
-    		 }else{
-    			 //If it is not the first try, use the integer given as input
-    			 Thread t = new ServerClient(newPort);
-		         t.start();
-		         createServer = 1;
-    		 }
-	      }catch(IOException e) {
-	    	 //If there is an error creating the socket, ask for a new port number
-	    	 //Then make sure to jump to the correct loop position
-	         System.out.println("Socket creation failed");
-	         System.out.println("Type a new port number: ");
-	         newPort = sc.nextInt();
-	         createServer = 0;
-	         portMethod = 1;
-	      }
-    	  
-      }
-      
-      //Create a loop to give option to the user
-      int choice = 9;
-      while (choice != 8){
-    	  //Print the choices to the user
-    	  System.out.println("Select a number with the preferred command:");
-    	  System.out.println("1.- Help");
-    	  System.out.println("2.- myIP");
-    	  System.out.println("3.- myPort");
-    	  System.out.println("4.- Connect to a server");
-    	  System.out.println("5.- List");
-    	  System.out.println("6.- Terminate");
-    	  System.out.println("7.- Send message");
-    	  System.out.println("8.- Exit");
-    	  choice = sc.nextInt();
-    	  
-    	  //Make a switch statement to handle the option selected
-    	  switch(choice){
-    	  case 1:
-    		  break;
-    	  case 2:
-    		  break;
-    	  case 3:
-    		  break;
-    	  case 4:
-    		  break;
-    	  case 5:
-    		  break;
-    	  case 6:
-    		  break;
-    	  case 7:
-    		  break;
-    	  case 8:
-    		  break;
-    	  default:
-    		  break;
-    	  }
-    	  
-      }
-      
-      //Close the input scanner
-      sc.close();
-   }
+/**
+ * Created by Maurice on 9/15/2016.
+ */
+
+public class SocketTest {
+
+    private List<Socket> list;
+
+    public static void main(String[] args) throws IOException {
+
+        startServer();
+        startSender();
+    }
+
+    public static void startSender() throws IOException{
+//        String host = "130.182.24.40";
+        String host = "localhost";
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Socket s = new Socket(host, 9090);
+                    System.out.println("bool: " + s.isConnected());
+                    System.out.println("external ip: " + s.getRemoteSocketAddress().toString());
+                    BufferedReader input =
+                            new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    String answer = input.readLine();
+                    System.out.println("answer: " + answer);
+//                    JOptionPane.showMessageDialog(null, answer);
+                    System.exit(0);
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    public static void startServer() {
+        (new Thread() {
+            @Override
+            public void run() {
+                try {
+                    ServerSocket listener = new ServerSocket(9090);
+                    try {
+                        while (true) {
+                            Socket socket = listener.accept();
+                            try {
+                                Scanner s = new Scanner(System.in);
+                                PrintWriter out =
+                                        new PrintWriter(socket.getOutputStream(), true);
+                                out.println(s.nextLine());
+                            } finally {
+                                socket.close();
+                            }
+                        }
+                    } finally {
+                        listener.close();
+                    }
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 }
