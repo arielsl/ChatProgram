2bc52cc58fced559b4c70aa08f0c3208ce3ae4af
arielsl <ariel.suarez1994@gmail.com>
2016-10-02 20:10:28 -0700

    Update Client.java

--- Classes/Client.java
+++ Classes/Client.java
@@ -2,31 +2,49 @@ package chat;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Client extends Thread{
 	private int port;
 	private String host;
+	InetAddress address;
 	
 	public Client(int portIn, String hostIn){
 		port = portIn;
 		host = hostIn;
+		try {
+			address = InetAddress.getByName(host);
+			System.out.println(address);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 		 
 	}
 	
 	 public void run() {
+		 System.out.println("Trying to create and connect a new socket to a server.");
 		 try {
-             Socket s = new Socket(host, port);
-             System.out.println("bool: " + s.isConnected());
-             System.out.println("external ip: " + s.getRemoteSocketAddress().toString());
-             BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-             String answer = input.readLine();
-             System.out.println("answer: " + answer);
-             s.close();
-             System.exit(0);
-         }
+             @SuppressWarnings("resource")
+			 Socket s = new Socket(address, port);
+             System.out.println("Connection status: " + s.isConnected());
+             System.out.println("Connected to IP: " + s.getRemoteSocketAddress().toString());
+             //Send the message to the server
+             OutputStream os = s.getOutputStream();
+             OutputStreamWriter osw = new OutputStreamWriter(os);
+             BufferedWriter bw = new BufferedWriter(osw);
+             
+             Scanner sc = new Scanner(System.in);
+             System.out.println("Write a message to send");
+             String message = sc.nextLine();
+   
+             bw.write(message);
+             bw.flush();
+             System.out.println("Message sent to the server : ");
+         	}
          catch (IOException e){
-             e.printStackTrace();
-         }
+             System.out.println("Client creation and connection failed");
+         	}
 	    }
 
 }
+
