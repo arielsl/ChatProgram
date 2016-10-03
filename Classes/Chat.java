58d1371323bec202f0769dfdedcd4b7b0af4696a
arielsl <ariel.suarez1994@gmail.com>
2016-10-02 20:06:52 -0700

    Update Chat.java

--- Classes/Chat.java
+++ Classes/Chat.java
@@ -1,7 +1,5 @@
 package chat;
 
-
-
 public class Chat {
 
 	public static void main(String[] args) {
@@ -9,10 +7,13 @@ public class Chat {
 		Thread t = new Server(port);
 		t.start();
 		System.out.println("Server Thread started");
-		Thread m = new Client(port, "localhost");
+		//Enter the IP here
+		//Host is given in the arguments as 6606
+		Thread m = new Client(6606 , "localhost");
 		m.start();
 		System.out.println("Client Thread Started");
 
 	}
 
 }
+
