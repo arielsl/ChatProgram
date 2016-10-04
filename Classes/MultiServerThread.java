759d34c54deb3be8e44defe5b415927d43519215
arielsl <ariel.suarez1994@gmail.com>
2016-10-03 17:12:51 -0700

    Update MultiServerThread.java

--- Classes/MultiServerThread.java
+++ Classes/MultiServerThread.java
@@ -8,22 +8,22 @@ public class MultiServerThread extends Thread{
 	 public MultiServerThread(Socket socket) {
 		 super("MultiServerThread");
 		 this.socket = socket;
-		 Chat.socketList.add(socket);
+		 
 	 }
 	 
 	 public void run() {
-		 System.out.println("Trying to read the message");
-		 try {
-			InputStream is = socket.getInputStream();
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(isr);
-			String message = br.readLine();
-			System.out.println("Message received from client " + socket.getRemoteSocketAddress().toString() + " is: " + message);
-			 
-		 } catch(Exception e){
-			 System.out.println("Message reader failed");
+		 while(true){
+			 try {
+				InputStream is = socket.getInputStream();
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader br = new BufferedReader(isr);
+				String message = br.readLine();
+				System.out.println("Message received from client " + socket.getRemoteSocketAddress().toString() + " is: " + message);
+				 
+			 } catch(Exception e){
+				 
+			 }
 		 }
 	 }
 
 }
-
