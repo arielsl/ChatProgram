998e7b5e81005001428e3d461ea984a28b8e841e
arielsl <ariel.suarez1994@gmail.com>
2016-10-03 17:12:02 -0700

    Update Chat.java

--- Classes/Chat.java
+++ Classes/Chat.java
@@ -9,6 +9,7 @@ public class Chat {
 	public static String ip;
 	public static int port;
 	static ArrayList<Socket> socketList = new ArrayList<Socket>();
+	private static Scanner sc;
 
 	public static void main(String[] args) {
 		port = Integer.parseInt(args[0]);
@@ -23,6 +24,7 @@ public class Chat {
 		System.out.println("Server created at address: " + ip + ":" + port);
 		
 		int choice = 10;
+		sc = new Scanner(System.in);
 		while(choice != 8){
 			System.out.println("Select a command with the given number: ");
 			System.out.println("1. Help");
@@ -33,36 +35,35 @@ public class Chat {
 			System.out.println("6. Terminate");
 			System.out.println("7. Send");
 			System.out.println("8. Exit");
-			Scanner sc = new Scanner(System.in);
-			choice = sc.nextInt();
-			switch(choice){
-			case 1: getHelp();
-				break;
-			case 2: try {
-					getIp();
-				} catch (Exception e) {
-					System.out.println("Could not retreat IP");
-				}
-				break;
-			case 3: getPort();
-				break;
-			case 4: connect();
-				break;
-			case 5: getList();
-				break;
-			case 6: terminate();
-				break;
-			case 7: send();
-				break;
-			case 8: sc.close(); 
-				endProgram();
-				break;
-			default: System.out.println("Not a valid command.");
-				break;
 			
-			}
+			choice = sc.nextInt();
+			
+			switch(choice){
+				case 1: getHelp();
+					break;
+				case 2: try {
+						getIp();
+					} catch (Exception e) {
+						System.out.println("Could not retreat IP");
+					}
+					break;
+				case 3: getPort();
+					break;
+				case 4: connect();
+					break;
+				case 5: getList();
+					break;
+				case 6: terminate();
+					break;
+				case 7: send();
+					break;
+				case 8: endProgram();
+					break;
+				default: System.out.println("Not a valid command.");
+					break;	
+				}
 		}
-		
+		sc.close();
 
     }
 	
@@ -102,15 +103,14 @@ public class Chat {
     }
     
     public static void connect(){
-    	Scanner sc = new Scanner(System.in);
+    	
     	System.out.println("Enter the IP of the server:");
-    	String serverIp = sc.nextLine();
+    	String serverIp = sc.next();
     	System.out.println("Enter the port of the server:");
     	int serverPort = sc.nextInt();
-    	Thread m = new Client(serverPort , serverIp);
-		m.start();
-		System.out.println("Client Thread Started with " + serverIp + ":" + serverPort);
-		sc.close();
+    	Client m = new Client(serverPort , serverIp);
+		m.connectSocket();
+		
     }
     
     public static void getList(){
@@ -123,20 +123,21 @@ public class Chat {
     }
     
     public static void terminate(){
-    	Scanner sc = new Scanner(System.in);
+    	
     	System.out.println("Select the index of the socket you wish to terminate:");
     	int index = sc.nextInt();
     	Socket s = socketList.get(index);
     	try {
 			s.close();
+			socketList.remove(index);
 		} catch (IOException e) {
 			System.out.println("Could not terminate connection.");
 		}
-    	sc.close();
+    	
     }
     
     public static void send(){
-    	Scanner sc = new Scanner(System.in);
+    	
     	System.out.println("Select the index of the socket you wish to send a message to:");
     	int index = sc.nextInt();
     	Socket s = socketList.get(index);
@@ -149,8 +150,8 @@ public class Chat {
 			 BufferedWriter bw = new BufferedWriter(osw);
 	      
 	        System.out.println("Write a message to send"); 
-	        String message = sc.nextLine();
-	        sc.close();
+	        String message = sc.next();
+	        
 	        
 	        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
 	        out.println(message);
